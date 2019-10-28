749e9545e57f5642d2f0bbd91078db8ef19b9cad
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-10-28 11:39:13 +0100

    add another split factor

--- src/power.cpp
+++ src/power.cpp
@@ -6,6 +6,7 @@
 #define PRETTY_SCALE_FACTOR 90UL
 #define MAX_AMPS_UINT (ALL_WHITE_AMPS * MAX_SCALE_FACTOR)
 #define PRETTY_AMPS_UINT (ALL_WHITE_AMPS * PRETTY_SCALE_FACTOR)
+#define SPLIT_FACTOR 0.56
 
 #define RED_POWER_DRAW 16
 #define GREEN_POWER_DRAW 11
@@ -29,7 +30,7 @@ namespace Power {
 			total_draw_2 += leds[i].g * GREEN_POWER_DRAW;
 			total_draw_2 += leds[i].b * BLUE_POWER_DRAW;
 		}
-		unsigned long total_draw = max(total_draw_1, total_draw_2);
+		unsigned long total_draw = (max(total_draw_1, total_draw_2) / SPLIT_FACTOR);
 		#else
 		unsigned long total_draw = 0;
 
@@ -40,6 +41,6 @@ namespace Power {
 		}
 		#endif
 		double draw_percent = ((double) PRETTY_AMPS_UINT) / (double)total_draw;
-		return draw_percent * 255UL;
+		return (draw_percent / 2) * 255UL;
 	}
 }
