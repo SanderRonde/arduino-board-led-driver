80ae3a0741ea3d92c77c627bf4c6f8bfaa86156b
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-10-28 11:39:16 +0100

    add vec???

--- include/vec.h
+++ include/vec.h
@@ -8,34 +8,22 @@ namespace Vec {
 	template<typename vec_type>
 	struct vec_part_t {
 		vec_part_t<vec_type>* next;
-		u_int16_t length : 5;
+		unsigned int length : 5;
 		vec_type items[VEC_PART_LEN];
 	};
 
-	template<typename vec_type>
-	struct vec_t {
-		long length;
-		vec_part_t<vec_type>* part;
+	template <class vec_type>
+	class Vector {
+		public:
+			long length;
+			Vec::vec_part_t<vec_type>* part;
+
+			void push(vec_type value);
+			vec_type get(int index);
+			void foreach(void (*fn)(vec_type value, int index));
+			void refresh();
+
+			Vector();
+			~Vector();
 	};
-
-	template<typename vec_type>
-	vec_t<vec_type>* vec_create();
-
-	template<typename vec_type>
-	void vec_push(vec_t<vec_type>* vec, vec_type value);
-
-	template<typename vec_type>
-	vec_type vec_get(vec_t<vec_type>* vec, int index);
-
-	template<typename vec_type>
-	void vec_foreach(vec_t<vec_type>* vec, void (*fn)(vec_type value, int index));
-
-	template<typename vec_type>
-	void vec_del(vec_t<vec_type>* vec);
-
-	template<typename vec_type>
-	void vec_free(vec_t<vec_type*>* vec);
-
-	template<typename vec_type>
-	Vec::vec_t<vec_type> vec_refresh(Vec::vec_t<vec_type>* vec, bool free);
 }
--- src/vec.cpp
+++ src/vec.cpp
@@ -12,29 +12,15 @@ namespace Vec {
 	}
 
 	template<typename vec_type>
-	Vec::vec_t<vec_type>* vec_create() {
-		Vec::vec_t<vec_type>* vec = (Vec::vec_t<vec_type>*) malloc(sizeof(Vec::vec_t<vec_type>));
-		vec->length = 0;
-		vec->part = vec_part_create();
-		return vec;
-	}
-
-	template<typename vec_type>
 	void vec_push_part(Vec::vec_part_t<vec_type>* vec, vec_type value) {
 		if (vec->length >= VEC_PART_LEN) {
 			if (vec->next == NULL) {
-				vec->next = vec_part_create();
+				vec->next = vec_part_create<vec_type>();
 			}
 			vec_push_part(vec->next);
 		} else {
 			vec->items[vec->length++] = value;
 		}
-	}
-
-	template<typename vec_type>
-	void vec_push(Vec::vec_t<vec_type>* vec, vec_type value) {
-		vec_push_part(vec->part, value);
-		vec->length++;
 	}
 
 	template<typename vec_type>
@@ -44,11 +30,6 @@ namespace Vec {
 			return vec_part_get(part->next, index - VEC_PART_LEN);
 		}
 		return part->items[index];
-	}
-
-	template<typename vec_type>
-	vec_type vec_get(Vec::vec_t<vec_type>* vec, int index) {
-		return vec_part_get(vec->part, index);
 	}
 
 	template<typename vec_type>
@@ -62,22 +43,11 @@ namespace Vec {
 	}
 
 	template<typename vec_type>
-	void vec_foreach(Vec::vec_t<vec_type>* vec, void (*fn)(vec_type value, int index)) {
-		vec_part_foreach(vec->part, fn);
-	}
-
-	template<typename vec_type>
 	void vec_del_part(Vec::vec_part_t<vec_type>* part) {
 		if (part->next) {
 			vec_del_part(part->next);
 		}
 		free(part);
-	}
-
-	template<typename vec_type>
-	void vec_del(Vec::vec_t<vec_type>* vec) {
-		vec_del_part(vec->part);
-		free(vec);
 	}
 
 	template<typename vec_type>
@@ -91,19 +61,37 @@ namespace Vec {
 		free(part);
 	}
 
-	template<typename vec_type>
-	void vec_free(Vec::vec_t<vec_type*>* vec) {
-		vec_del_part(vec->part);
-		free(vec);
+	template <class vec_type>
+	Vector<vec_type>::Vector() {
+		length = 0;
+		part = vec_part_create<vec_type>();
 	}
 
-	template<typename vec_type>
-	Vec::vec_t<vec_type> vec_refresh(Vec::vec_t<vec_type>* vec, bool free) {
-		if (free) {
-			vec_free(vec);
-		} else {
-			vec_del(vec);
-		}
-		return vec_create();
+	template <class vec_type>
+	void Vector<vec_type>::push(vec_type value) {
+		vec_push_part(part, value);
+		length++;
+	}
+
+	template <class vec_type>
+	vec_type Vector<vec_type>::get(int index) {
+		return vec_part_get(part, index);
+	}
+
+	template <class vec_type>
+	void Vector<vec_type>::foreach(void (*fn)(vec_type value, int index)) {
+		vec_part_foreach(part, fn);
+	}
+
+	template <class vec_type>
+	void Vector<vec_type>::refresh() {
+		vec_del_part<vec_type>(part);
+		part = vec_part_create<vec_type>();
+		length = 0;
+	}
+
+	template <class vec_type>
+	Vector<vec_type>::~Vector() {
+		vec_del_part(part);
 	}
 }
