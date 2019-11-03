e4d044a36ba3faa9b65c06fd31597539005713b1
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-11-03 19:26:47 +0100

    calculate leds per strip

--- include/globals.h
+++ include/globals.h
@@ -7,7 +7,8 @@
 #define LED_PIN 7
 #define LEDS_PER_M 60
 #define METERS_PER_STRIP 5
+#define LEDS_PER_STRIP (LEDS_PER_M * METERS_PER_STRIP)
 #define STRIPS 3
-#define NUM_LEDS ((LEDS_PER_M * METERS_PER_STRIP) * STRIPS)
+#define NUM_LEDS (LEDS_PER_STRIP * STRIPS)
 
 extern CRGB leds[NUM_LEDS];
